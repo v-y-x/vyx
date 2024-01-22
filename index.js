2546c42b84b0417f10cc1ae5d3b5fd03d2dac759
balka-n <harun.dzambo@gmail.com>
2024-01-22 13:22:03 +0100

    oga boga

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-function displayText() {
+function AboutText() {
     var text = document.getElementById("abouttext");
     text.style.display = "block";
+}
+function MusicText() {
+    var text = document.getElementById("musictext");
+    text.style.display = "block";
 }  
+function SocialsText() {
+    var text = document.getElementById("socialstext");
+    text.style.display = "block";
+}
+function removeA() {
+    var displayParagraph = document.getElementById("abouttext");
+
+    displayParagraph.style.display = "none";
+}
+function removeM() {
+    var displayParagraph = document.getElementById("musictext");
+
+    displayParagraph.style.display = "none";
+}
+function removeS() {
+    var displayParagraph = document.getElementById("socialstext");
+
+    displayParagraph.style.display = "none";
+}
